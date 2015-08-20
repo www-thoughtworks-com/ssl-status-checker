a6e864c00a9d35bcb781b983c789b01fee1c3979
Ama <aasare@thoughtworks.com>
2015-08-20 11:06:38 +0100

    Ama/Medina/Darren #2516: Add tw.com urls spreadsheet to repository

--- server/index.js
+++ server/index.js
@@ -1,44 +1,44 @@
 var express = require('express');
 var app = express();
 var mixedContent = require('./index');
-var spreadsheetURL = 'https://docs.google.com/spreadsheets/d/1sN0uGj5JNtvhU76X9NZ2gkU5rxzAMcEMA_dXnj0L5x8/pubhtml';
+var spreadsheetURL = 'https://docs.google.com/spreadsheets/d/1eLZeljiEggkjxIp4ZB2hd4FrSIkhjN_efRArRkswumA/pubhtml?gid=0&single=true';
 var tabletop = require('tabletop');
 var assetParser = require('./asset-parser');
 var port = process.env.PORT || 3000;
 
 app.set('view engine', 'jade');
-app.set('views', __dirname +'/views');
+app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 
 // home
 app.get('/', function (req, res) {
-  tabletop.init({
-    key: spreadsheetURL,
+    tabletop.init({
+        key: spreadsheetURL,
     callback: function(d) {
-      res.set('Content-Type', 'text/html');
+            res.set('Content-Type', 'text/html');
       res.render('index', { title: 'Status', message: 'Status', data: JSON.stringify(d)});
-    },
-    simpleSheet: true
-  })
+        },
+        simpleSheet: true
+    })
 });
 
 // API status for a given url
 app.get('/api/:url', function (req, res) {
-  assetParser(req.params.url, function(link, data) {
-    res.json({url: link, data: data});
-  }, function(e) {
-    res.json(e);
-  })
+    assetParser(req.params.url, function (link, data) {
+        res.json({url: link, data: data});
+    }, function (e) {
+        res.json(e);
+    })
 });
 
 
 module.exports = {
-  start: function() {
-    var server = app.listen(port, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('App listening at http://%s:%s', host, port);
-});
-  }
+    start: function () {
+        var server = app.listen(port, function () {
+            var host = server.address().address;
+            var port = server.address().port;
+            console.log('App listening at http://%s:%s', host, port);
+        });
+    }
 }
